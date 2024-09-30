e0294fbc32d776fe574fb82c3113c8f9531487e8
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-09-30 13:42:59 +0530

    Update

---
+++ factorialNum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int factorialNum(){
+    int n, i, factorial = 1;
+    printf("Input the number: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Factorial of a negative number doesn't exist.\n");
+        
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial *= i;
+            
+        }
+        printf("The Factorial of %d is: %llu\n", n, factorial);
+        
+    }
+    return 0;
+}
+int main() {
+    int a;
+    a=factorialNum();
+    printf("Enter num:%d", a);
+    return 0;
+}
