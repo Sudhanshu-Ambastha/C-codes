92317ae35f2b4788f8515116dfef3cde7df10f30
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2024-11-01 19:54:17 +0530

    Update IsPalindrome.c

--- IsPalindrome.c
+++ IsPalindrome.c
@@ -1,52 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    char ch, str[100];  
-    int num, digit1, digit2, digit3, reversed, isPalindrome = 1, length = 0;
+int main(){
+    char str[100];
+    int i, length;
+    int isPalindrome = 1;
 
-    printf("Choose type (n for num / c for character): ");
-    scanf(" %c", &ch);  // Added space before %c to ignore the newline left by the previous input
+    printf("Enter a string: ");
+    scanf("%s", str);
 
-    switch(ch) {
-        case 'n':  
-            printf("Enter a 3-digit number: ");
-            scanf("%d", &num);
+    length = strlen(str);
 
-            digit1 = num % 10;        
-            digit2 = (num / 10) % 10;  
-            digit3 = num / 100;      
-            
-            reversed = digit1 * 100 + digit2 * 10 + digit3;
-
-            if (num == reversed)
-                printf("%d is a palindrome.\n", num);
-            else
-                printf("%d is not a palindrome.\n", num);
+    for (i = 0; i < length / 2; i++){
+        if (str[i] != str[length - 1 - i]){
+            isPalindrome = 0;
             break;
+        }
+    }
 
-        case 'c':  
-            printf("Enter a string: ");
-            scanf("%s", str);  
-            
-            while (str[length] != '\0') {
-                length++;
-            }
-
-            for (int i = 0; i < length / 2; i++) {
-                if (str[i] != str[length - 1 - i]) {
-                    isPalindrome = 0; 
-                    break;
-                }
-            }
-            
-            if (isPalindrome)
-                printf("%s is a palindrome.\n", str);
-            else
-                printf("%s is not a palindrome.\n", str);
-            break;
-
-        default:
-            printf("Not a valid choice.\n");
+    if (isPalindrome){
+        printf("The string is a palindrome.\n");
+    }else{
+        printf("The string is not a palindrome.\n");
     }
 
     return 0;
