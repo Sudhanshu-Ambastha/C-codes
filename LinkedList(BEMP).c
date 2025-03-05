8410b8e252e04b3f4c3ad5f55b437a9e3f663bdd
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2025-03-06 04:36:57 +0530

    Update LinkedList(BEMP).c

--- LinkedList(BEMP).c
+++ LinkedList(BEMP).c
@@ -6,47 +6,31 @@ struct Node {
     struct Node* next;
 };
 
-int getPosition(struct Node* head, int data) {
-    struct Node* temp = head;
-    int position = 1;
-
-    while (temp != NULL) {
-        if (temp->data == data) {
-            return position;
-        }
-        temp = temp->next;
-        position++;
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation failed!\n");
+        return NULL;
     }
-
-    return -1;
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
 struct Node* insertAtBeginning(struct Node* head, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed!\n");
-        return head;
-    }
-    newNode->data = data;
+    struct Node* newNode = createNode(data);
+    if (!newNode) return head; 
     newNode->next = head;
     return newNode;
 }
 
 struct Node* insertAtEnd(struct Node* head, int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed!\n");
-        return head;
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-
-    if (head == NULL) {
-        return newNode;
-    }
+    struct Node* newNode = createNode(data);
+    if (!newNode) return head;
+    if (!head) return newNode;
 
     struct Node* temp = head;
-    while (temp->next != NULL) {
+    while (temp->next) {
         temp = temp->next;
     }
     temp->next = newNode;
@@ -59,24 +43,19 @@ struct Node* insertAtPosition(struct Node* head, int data, int position) {
         return head;
     }
 
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed!\n");
-        return head;
-    }
-    newNode->data = data;
-
+    struct Node* newNode = createNode(data);
+    if (!newNode) return head;
     if (position == 1) {
         newNode->next = head;
         return newNode;
     }
 
     struct Node* temp = head;
-    for (int i = 1; i < position - 1 && temp != NULL; i++) {
+    for (int i = 1; i < position - 1 && temp; i++) {
         temp = temp->next;
     }
 
-    if (temp == NULL) {
+    if (!temp) {
         printf("Position out of bounds!\n");
         free(newNode);
         return head;
@@ -88,24 +67,27 @@ struct Node* insertAtPosition(struct Node* head, int data, int position) {
 }
 
 struct Node* insertAtMid(struct Node* head, int data) {
-    if (head == NULL) {
-        return insertAtBeginning(head, data);
-    }
+    if (!head) return insertAtBeginning(head, data);
 
     struct Node* slow = head;
     struct Node* fast = head;
 
-    while (fast->next != NULL && fast->next->next != NULL) {
+    while (fast->next && fast->next->next) {
         slow = slow->next;
         fast = fast->next->next;
     }
-    int midPosition = getPosition(head, slow->data) + 1;
-    return insertAtPosition(head, data, midPosition);
+    int position = 1;
+    struct Node* temp = head;
+    while(temp != slow){
+        temp = temp->next;
+        position++;
+    }
+    return insertAtPosition(head, data, position+1);
 }
 
 void displayList(struct Node* head) {
     struct Node* temp = head;
-    while (temp != NULL) {
+    while (temp) {
         printf("%d -> ", temp->data);
         temp = temp->next;
     }
@@ -129,24 +111,24 @@ int main() {
 
         switch (choice) {
             case 1:
-                printf("Enter data to insert: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
                 head = insertAtBeginning(head, data);
                 break;
             case 2:
-                printf("Enter data to insert: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
                 head = insertAtEnd(head, data);
                 break;
             case 3:
-                printf("Enter data to insert: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
-                printf("Enter position to insert: ");
+                printf("Enter position: ");
                 scanf("%d", &position);
                 head = insertAtPosition(head, data, position);
                 break;
             case 4:
-                printf("Enter data to insert: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
                 head = insertAtMid(head, data);
                 break;
@@ -159,6 +141,5 @@ int main() {
                 printf("Invalid choice!\n");
         }
     }
-
     return 0;
 }
